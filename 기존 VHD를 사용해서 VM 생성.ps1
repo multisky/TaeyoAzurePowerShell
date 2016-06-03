31529154f06b542869823358a455e031d59debf4
taeyo <taeyokim@gmail.com>
2016-06-03 20:47:18 +0900

    변수 설정 부분 보완

--- 기존 VHD를 사용해서 VM 생성.ps1
+++ 기존 VHD를 사용해서 VM 생성.ps1
@@ -3,17 +3,19 @@ Login-AzureRmAccount
 
 ###########################################################
 ## 이 부분은 직접 자신에 맞게 변수값을 설정해 줘야 한다.
-$subscriptionName = "BizSpark"
-$resourceGroupName = "rg-jw-taeyo-study"
-$location = "japan west"
-$VNetName = "vn-jw-taeyo-test"
+$subscriptionName = "<구독이름>" # "MyMSDN"
+$resourceGroupName = "<리소스 그룹 이름"> # "rg-jw-taeyo-study"
+$location = "<위치">  # japan west"
+$VNetName = "<가상 네트워크 이름>"  # vn-jw-taeyo-test"
 
 ## 만들 VM의 이름과 크기
-$vmname = "PlzRemove"
-$vmSize = "Standard_A2"
+$vmname = "<만들어질 VM 이름">     # "PlzRemove"
+$vmSize = "<만들어질 VM 크기">     # "Standard_A2"
 
 ## VHD 경로
-$osDiskVhdUri = "https://taeyostudy.blob.core.windows.net/vhds/TestVMCanRemove2016117155451.vhd"
+$osDiskVhdUri = "<VHD 파일의 FQDN 경로>" 
+# "https://taeyostudy.blob.core.windows.net/vhds/TestVMCanRemove2016117155451.vhd"
+
 ###########################################################
 $osDiskName = $vmName + "_osDisk"
 $interfaceName = $vmname + "_Interface"
