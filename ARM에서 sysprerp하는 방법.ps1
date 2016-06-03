5ccbc0fb0aafec3cc37f75fe9a75f2b80bdd8dc3
taeyo <taeyokim@gmail.com>
2016-06-03 20:18:09 +0900

    ARM에서의 sysprep 파워쉘 추가

---
+++ ARM에서 sysprerp하는 방법.ps1
@@ -0,0 +1,24 @@
+﻿## 로그인
+Login-AzureRmAccount
+
+## 구독 설정
+$subscr = "BizSpark"
+$resourceGroupName = "rg-jw-taeyo-study"
+$vmname = "PlzRemove"
+$imageName = $vmname + "_CapturedImage"
+
+Get-AzureRmSubscription –SubscriptionName $subscr | Select-AzureRmSubscription
+
+# Deallocate the virtual machine
+Stop-AzureRmVM -ResourceGroupName $resourceGroupName -Name $vmname 
+
+# Set the Generalized state to the virtual machine
+Set-AzureRmVM -ResourceGroupName $resourceGroupName -Name $vmname -Generalized  
+
+# Capture the image to storage account.
+Save-AzureRmVMImage -ResourceGroupName $resourceGroupName -VMName $vmname `
+    -DestinationContainerName 'mytemplates' -VHDNamePrefix 'template' `
+    -Path C:\temp\capturevmtest\SampleTemplate.json  
+
+
+
